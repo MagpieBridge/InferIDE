c18f27392d3b0464998e591e68bd4289f7ba3871
Linghui Luo <linghui@outlook.de>
2020-05-15 21:00:18 +0200

    more to play

--- src/main/java/magpiebridge/finfer/InferIDE.java
+++ src/main/java/magpiebridge/finfer/InferIDE.java
@@ -1,12 +1,16 @@
 package magpiebridge.finfer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 import magpiebridge.core.IProjectService;
 import magpiebridge.core.MagpieServer;
 import magpiebridge.core.ServerAnalysis;
 import magpiebridge.core.ServerConfiguration;
 import magpiebridge.core.ToolAnalysis;
+import magpiebridge.core.analysis.configuration.ConfigurationOption;
+import magpiebridge.core.analysis.configuration.OptionType;
 import magpiebridge.projectservice.java.JavaProjectService;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 
@@ -15,6 +19,8 @@ public class InferIDE {
     Supplier<MagpieServer> createServer = () -> {
       ServerConfiguration config = new ServerConfiguration();
       config.setDoAnalysisByIdle(true, 4 * 60 * 1000);
+      config.setReportFalsePositive(true);
+      config.setReportConfusion(true);
       config.setShowConfigurationPage(true);
       MagpieServer server = new MagpieServer(config);
       String language = "java";
@@ -23,6 +29,24 @@ public class InferIDE {
       ToolAnalysis analysis = new InferServerAnalysis();
       Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
       server.addAnalysis(either, language);
+
+      ToolAnalysis analysis2 = new InferServerAnalysis() {
+        @Override
+        public List<ConfigurationOption> getConfigurationOptions() {
+          List<ConfigurationOption> commands = new ArrayList<>();
+          ConfigurationOption command1 =
+              new ConfigurationOption("run command: infer capture", OptionType.checkbox);
+          ConfigurationOption command2 =
+              new ConfigurationOption("run command: infer capture --reactive", OptionType.checkbox);
+          ConfigurationOption command3 = new ConfigurationOption("run command: ", OptionType.text);
+          commands.add(command1);
+          commands.add(command2);
+          commands.add(command3);
+          return commands;
+        }
+      };
+      Either<ServerAnalysis, ToolAnalysis> either2 = Either.forRight(analysis2);
+      server.addAnalysis(either2, language);
       return server;
     };
     // createServer.get().launchOnStdio();
