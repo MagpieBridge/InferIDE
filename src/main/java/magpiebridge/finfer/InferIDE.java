6115b8acc30ff69c5737358468f0077d71a7ee5a
Linghui Luo <linghui@outlook.de>
2020-05-15 12:30:21 +0200

    add configuration

--- src/main/java/magpiebridge/finfer/InferIDE.java
+++ src/main/java/magpiebridge/finfer/InferIDE.java
@@ -1,25 +1,31 @@
 package magpiebridge.finfer;
 
-import org.eclipse.lsp4j.jsonrpc.messages.Either;
-
+import java.io.IOException;
+import java.util.function.Supplier;
 import magpiebridge.core.IProjectService;
 import magpiebridge.core.MagpieServer;
 import magpiebridge.core.ServerAnalysis;
 import magpiebridge.core.ServerConfiguration;
 import magpiebridge.core.ToolAnalysis;
 import magpiebridge.projectservice.java.JavaProjectService;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
 
 public class InferIDE {
-  public static void main(String... args) {
-    ServerConfiguration config = new ServerConfiguration();
-    MagpieServer server = new MagpieServer(config);
-    String language = "java";
-    IProjectService javaProjectService = new JavaProjectService();
-    server.addProjectService(language, javaProjectService);
-    ServerAnalysis analysis = new InferServerAnalysis();
-    Either<ServerAnalysis, ToolAnalysis> either = Either.forLeft(analysis);
-    server.addAnalysis(either, language);
-    //server.launchOnStdio();
-    server.launchOnSocketPort(5007);
+  public static void main(String... args) throws IOException, InterruptedException {
+    Supplier<MagpieServer> createServer = () -> {
+      ServerConfiguration config = new ServerConfiguration();
+      config.setDoAnalysisByIdle(true, 4 * 60 * 1000);
+      config.setShowConfigurationPage(true);
+      MagpieServer server = new MagpieServer(config);
+      String language = "java";
+      IProjectService javaProjectService = new JavaProjectService();
+      server.addProjectService(language, javaProjectService);
+      ToolAnalysis analysis = new InferServerAnalysis();
+      Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
+      server.addAnalysis(either, language);
+      return server;
+    };
+    // createServer.get().launchOnStdio();
+    MagpieServer.launchOnSocketPort(5007, createServer);
   }
 }
--- src/main/java/magpiebridge/finfer/InferServerAnalysis.java
+++ src/main/java/magpiebridge/finfer/InferServerAnalysis.java
@@ -1,36 +1,45 @@
 package magpiebridge.finfer;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+import com.ibm.wala.cast.tree.CAstSourcePositionMap.Position;
 import com.ibm.wala.classLoader.Module;
-
+import com.ibm.wala.util.collections.Pair;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-
-import org.eclipse.lsp4j.DiagnosticSeverity;
-
+import java.util.Map;
+import java.util.stream.Collectors;
+import magpiebridge.core.AnalysisConsumer;
 import magpiebridge.core.AnalysisResult;
 import magpiebridge.core.Kind;
 import magpiebridge.core.MagpieServer;
-import magpiebridge.core.ServerAnalysis;
+import magpiebridge.core.ProjectType;
+import magpiebridge.core.ToolAnalysis;
+import magpiebridge.core.analysis.configuration.ConfigurationAction;
+import magpiebridge.core.analysis.configuration.ConfigurationOption;
+import magpiebridge.core.analysis.configuration.OptionType;
 import magpiebridge.projectservice.java.JavaProjectService;
-import magpiebridge.util.SourceCodeInfo;
 import magpiebridge.util.SourceCodePositionFinder;
+import org.apache.http.NameValuePair;
+import org.eclipse.lsp4j.DiagnosticSeverity;
 
-public class InferServerAnalysis implements ServerAnalysis {
+public class InferServerAnalysis implements ToolAnalysis {
   private String rootPath;
+  private String reportPath;
+  private ProjectType projectType;
+  private boolean firstTime = true;
+  private static boolean showTrace = false;
+  private String command;
 
   @Override
   public String source() {
@@ -38,50 +47,137 @@ public class InferServerAnalysis implements ServerAnalysis {
   }
 
   @Override
-  public void analyze(Collection<? extends Module> files, MagpieServer server, boolean rerun) {
-    JavaProjectService ps = (JavaProjectService) server.getProjectService("java").get();
-    if (ps.getRootPath().isPresent()) {
-      this.rootPath = ps.getRootPath().get().toString();
-      Collection<AnalysisResult> results = read(new File("report.json"));
-      server.consume(results, source());
+  public void analyze(Collection<? extends Module> files, AnalysisConsumer consumer,
+      boolean rerun) {
+    if (consumer instanceof MagpieServer) {
+      MagpieServer server = (MagpieServer) consumer;
+      if (this.rootPath == null) {
+        JavaProjectService ps = (JavaProjectService) server.getProjectService("java").get();
+        if (ps.getRootPath().isPresent()) {
+          this.rootPath = ps.getRootPath().get().toString();
+          this.projectType = ps.getProjectType();
+          this.reportPath =
+              this.rootPath + File.separator + "infer-out" + File.separator + "report.json";
+        }
+      }
+      if (rerun && this.rootPath != null) {
+        server.submittNewTask(() -> {
+          try {
+            File report = new File(InferServerAnalysis.this.reportPath);
+            if (report.exists())
+              report.delete();
+            Process runInfer = this.runCommand(new File(InferServerAnalysis.this.rootPath));
+            if (runInfer.waitFor() == 0) {
+              File file = new File(InferServerAnalysis.this.reportPath);
+              if (file.exists()) {
+                Collection<AnalysisResult> results = convertToolOutput();
+                server.consume(results, source());
+              }
+            }
+          } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+          }
+        });
+      }
     }
   }
 
-  public Collection<AnalysisResult> read(File report) {
-    Collection<AnalysisResult> res = new ArrayList<AnalysisResult>();
 
+  private String getToolBuildCmdWithClean() {
+    if (this.projectType.equals(ProjectType.Maven))
+      return "mvn clean compile";
+    if (this.projectType.equals(ProjectType.Gradle))
+      return "";
+    return "";
+  }
+
+  private String getToolBuildCmd() {
+    if (this.projectType.equals(ProjectType.Maven))
+      return "mvn compile";
+    if (this.projectType.equals(ProjectType.Gradle))
+      return "";
+    return "";
+  }
+
+  @Override
+  public String[] getCommand() {
+    String cmd = "infer run --reactive -- ";
+    if (firstTime) {
+      firstTime = false;
+      cmd = cmd + getToolBuildCmdWithClean();
+    } else
+      cmd = cmd + getToolBuildCmd();
+    return cmd.split(" ");
+  }
+
+  @Override
+  public Collection<AnalysisResult> convertToolOutput() {
+    Collection<AnalysisResult> res = new ArrayList<AnalysisResult>();
     try {
       JsonParser parser = new JsonParser();
-      JsonArray bugs =parser.parse(new FileReader(report)).getAsJsonArray();
+      JsonArray bugs = parser.parse(new FileReader(new File(this.reportPath))).getAsJsonArray();
       for (int i = 0; i < bugs.size(); i++) {
         JsonObject bug = bugs.get(i).getAsJsonObject();
         String bugType = bug.get("bug_type").getAsString();
         String qualifier = bug.get("qualifier").getAsString();
         int line = bug.get("line").getAsInt();
         String file = this.rootPath + File.separator + bug.get("file").getAsString();
-        SourceCodeInfo info = SourceCodePositionFinder.findCode(new File(file), line);
-        URL url = new URL("file://"+ file);
-        SourceCodePosition pos = new SourceCodePosition(url, line, info.range.getStart().getCharacter(), line,
-            info.range.getEnd().getCharacter());
+        Position pos = SourceCodePositionFinder.findCode(new File(file), line).toPosition();
         String msg = bugType + ": " + qualifier;
-        AnalysisResult rbug
-            = new InferResult(Kind.Diagnostic, pos, msg, new ArrayList<>(), DiagnosticSeverity.Error, null, null);
+        JsonArray trace = bug.get("bug_trace").getAsJsonArray();
+        ArrayList<Pair<Position, String>> traceList = new ArrayList<Pair<Position, String>>();
+        if (showTrace) {
+          for (int j = 0; j < trace.size(); j++) {
+            JsonObject step = trace.get(j).getAsJsonObject();
+            String stepFile = this.rootPath + File.separator + step.get("filename").getAsString();
+            if (new File(stepFile).exists()) {
+              int stepLine = step.get("line_number").getAsInt();
+              String stepDescription = step.get("description").getAsString();
+              Position stepPos =
+                  SourceCodePositionFinder.findCode(new File(stepFile), stepLine).toPosition();
+              Pair<Position, String> pair = Pair.make(stepPos, stepDescription);
+              traceList.add(pair);
+            }
+          }
+        }
+        AnalysisResult rbug = new InferResult(Kind.Diagnostic, pos, msg, traceList,
+            DiagnosticSeverity.Error, null, null);
         res.add(rbug);
       }
     } catch (JsonIOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (JsonSyntaxException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     return res;
   }
+
+  @Override
+  public List<ConfigurationOption> getConfigurationOptions() {
+    List<ConfigurationOption> commands = new ArrayList<>();
+    ConfigurationOption command1 =
+        new ConfigurationOption("run command: infer run", OptionType.checkbox);
+    ConfigurationOption command2 =
+        new ConfigurationOption("run command: infer run --reactive", OptionType.checkbox);
+    commands.add(command1);
+    commands.add(command2);
+    return commands;
+  }
+
+  @Override
+  public List<ConfigurationAction> getConfiguredActions() {
+    return Collections.EMPTY_LIST;
+  }
+
+  @Override
+  public void configure(List<ConfigurationOption> configuration) {
+    for (ConfigurationOption o : configuration)
+      if (o.getValueAsBoolean())
+        command = o.getName().split(": ")[1];
+  }
+
+
 
 }
